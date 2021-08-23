56d0b7a6f5f50e3ade5f97caab53d3fe97e5c91c
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-23 18:56:53 +0900

    ✨  遅延実行

--- sample/index.js
+++ sample/index.js
@@ -1,2 +1,8 @@
-console.log(__dirname);
-console.log(__filename);
+setTimeout(() => {
+  console.log("setTimeout()");
+}, 100);
+
+console.log("global"); // settimeoutが後から実行される
+
+var end = (new Date()).getTime() + 3000;  // 3秒間遅延させる。
+while ((new Date()).getTime() < end) { }
