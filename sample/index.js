5b144e91ebf2735e203616e29868e23234905df0
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-24 23:31:42 +0900

    ✨  コンソールログ出力

--- sample/index.js
+++ sample/index.js
@@ -1,16 +1,5 @@
-setTimeout(() => {
-  console.log("setTimeout()");  // 実行順4番: 0.1秒遅延しているため
-}, 100);
+console.log("hello");
+console.trace("world"); // スタックトレースの出力
+console.trace("Message: %o", { hello: "world" });  // JSONオブジェクトの展開
 
-setImmediate(() => {
-  console.log("setImmediate()"); // 実行順3番: キューの後の優先となるため
-});
-
-process.nextTick(() => {
-  console.log("nextTick()");  // 実行順1番:キューが優先されるため
-});
-
-Promise.resolve().then(() => {
-  console.log("Promise.resolve().then()"); // 実行順2番:キューが優先されるため
-});
 
