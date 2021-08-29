97c1ca2e63d5a55ce616c38b921de7d4848c52f5
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-29 21:51:52 +0900

    ✨ 実行中プログラムに関する情報/操作

--- sample/index.js
+++ sample/index.js
@@ -1,5 +1,14 @@
-console.log("hello");
-console.trace("world"); // スタックトレースの出力
-console.trace("Message: %o", { hello: "world" });  // JSONオブジェクトの展開
+// 環境変数
+for (var i = 0; process.argv.length; i++) {
+    console.log(`{i} : ${process.argv[i]}`);
+}
 
+// 実行時引数
+console.log(JSON.stringify(process.env, null, 2));
 
+// カレントディレクトリ
+console.log(`cwd() : ${process.cwd()}`);
+console.log(`dirname: ${__dirname}`);
+
+// 実行環境
+console.log(process.platform);
