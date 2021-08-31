aaa7203d78388634d91727d6965757a8c8b4ab8c
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-31 15:04:46 +0900

    ✨ functionを使ったクラス継承

--- sample/index.js
+++ sample/index.js
@@ -1,30 +1,28 @@
-// 親クラスの定義
-var Car = class {
-    constructor(name) {
-      this.name = name;
-    }
-  
-    drive() {
-      console.log('zoom zoom ...');
-    }
-  };
-  
-  // 子クラスの定義
-  var Lamborghini = class extends Car {
-    constructor(name) {
-      super(name);
-    }
-  
-    echo() {
-      super.drive();
-    }
-  
-    drive() {
-      console.log(`fire ${this.name} !!`);
-    }
-  };
-  
-  var car = new Lamborghini('lamborghini');
-  car.echo();
-  car.drive();
-  
+var util = require("util");
+
+// 親クラス
+var Car = function (name) {
+  this.name = name;
+};
+
+Car.prototype.drive = function () {
+  console.log("zoom zoom ...");
+};
+
+// 子クラス
+var Lamborghini = function (name) {
+  Lamborghini.super_.call(this, name);
+};
+util.inherits(Lamborghini, Car);
+
+Lamborghini.prototype.echo = function () {
+  Lamborghini.super_.prototype.drive.call(this);
+};
+
+Lamborghini.prototype.drive = function () {
+  console.log(`fire ${this.name}`);
+};
+
+var car = new Lamborghini("lamborghini");
+car.echo();
+car.drive();
