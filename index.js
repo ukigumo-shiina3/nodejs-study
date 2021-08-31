dfb7cfe9415ed6042040b18d139e34f9d59d74eb
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-31 19:21:23 +0900

    ✨ モジュール化

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-var message = require("./module");
-console.log(message);
+var { echo, area } = require("./methods");
+var Lamborghini = require("./lamborghini");
+var config = require("./config");
+
+echo("Hello World !");
+console.log(area(3, 5));
+
+var car = new Lamborghini("lamborghini");
+car.echo();
+car.drive();
+
+console.log(JSON.stringify(config));
